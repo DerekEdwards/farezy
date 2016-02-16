e24fdec0009dbfff7f8dfc7fdde5522f8670a77a
Derek Edwards <dedwards8@gmail.com>
2016-02-16 17:57:34 -0500

    #5 Can add new Day Passes on Edit Page

--- app/controllers/day_passes_controller.rb
+++ app/controllers/day_passes_controller.rb
@@ -1,10 +1,10 @@
 class DayPassesController < ApplicationController
 
   before_action :set_city_and_day_pass, only: [:update]
+  before_action :set_city, only: [:create]
 
   def update
-    #Update City Attributes
-    puts params.ai
+    #Update Day Pass
     new_attributes = {days: params[:days], cost: params[:cost]}
     @day_pass.update_attributes(new_attributes)
 
@@ -13,12 +13,25 @@ class DayPassesController < ApplicationController
     end
   end
 
+  def create
+    #Create New Day Pass
+    new_attributes = {days: params[:days], cost: params[:cost], city: @city}
+    @day_pass = DayPass.create(new_attributes)
+
+    respond_to do |format|
+      format.json { render json: {result: 200}}
+    end
+  end
 
   private
 
   def set_city_and_day_pass
     @city = City.find(params[:city_id])
     @day_pass = DayPass.find(params[:id])
+  end
+
+  def set_city
+    @city = City.find(params[:city_id])
   end
 
 end
