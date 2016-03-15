0cf4b0ab127489981e7b9f7fffc063aa107871a4
Derek Edwards <dedwards8@gmail.com>
2016-03-14 22:38:58 -0400

    #21 Show mail errors and turn on mail settings when in development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Setup for Devise Emails
   config.action_mailer.default_url_options = { host: ENV['host'] }
